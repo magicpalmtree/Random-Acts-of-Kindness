56adec7b80975ef2f879361391b655cc7c9a2a8a
magicpalmtree <magicpalmtree.space@gmail.com>
2017-09-24 16:53:27 -0500

    Added user authentication using passport.js and express-session. Currently user can sign up and post stories. Login page is the next step. Story will be associated with user id.

--- server.js
+++ server.js
@@ -22,6 +22,24 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("public"));
 
+// User authentication
+var session = require("express-session");
+var passport = require("passport");
+
+// from express session github
+
+app.use(session({
+  secret: 'jklsjflsfds',
+  resave: false,
+  saveUninitialized: false,
+  //cookie: { secure: true }
+}));
+
+// passport github
+// initialize and integrate with express session
+app.use(passport.initialize());
+app.use(passport.session());
+
 // -------------------------------------------------
 mongoose.Promise = global.Promise;
 // MongoDB Configuration configuration (Change this URL to your own DB)
@@ -38,10 +56,10 @@ db.once("open", function() {
 
 // -------------------------------------------------
 
-// // Main "/" Route. This will redirect the user to our rendered React application
-// app.get("/", function(req, res) {
-//   res.sendFile(__dirname + "/public/index.html");
-// });
+// // Main "/stories" Route. This will redirect the user to our rendered React application
+ app.get("/stories", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // // This is the route we will send GET requests to retrieve our most recent search data.
 // // We will call this route the moment our page gets rendered
@@ -70,27 +88,50 @@ app.post("/users", function(req, res) {
     picUrl: req.body.picUrl,
     email: req.body.email,
     date: Date.now()
-  })
+  });
 
   console.log("BODY: " + req.body);
 
   user.save(function(err, doc) {
-      if(err) { 
+    if(err) { 
         console.log(err);
-      } else {
-      res.send(doc);
-      }
+    } else {
+      //res.send(doc);
+    
+      const userId = doc._id;
+      console.log("user id = " + userId);
+      req.login(userId, function(err) {
+          res.redirect("/stories");
+      });
+    }
   });
 });
 
+// write user id (serialize) to the session
+passport.serializeUser(function(userId, done) {
+    done(null, userId);
+});
+
+// read user id from the session
+passport.deserializeUser(function(userId, done) {
+  //User.findById(userId, function (err, user) {
+    done(null, userId);
+  //});
+});
+
+
 app.post("/stories", function(req, res) {
+
+  // get user id stored in the sesion
+  console.log(req.user);
+  console.log(req.isAuthenticated());
 
   // Here we'll save the location based on the JSON input.
   // We'll use Date.now() to always get the current date time
   var story = new Story({
-    postedBy: "Ebru",
+    postedBy: req.user,
     title: req.body.title,
-    longV: req.body.longV,
+    longVersion: req.body.longV,
     date: Date.now()
   })
 
@@ -100,8 +141,9 @@ app.post("/stories", function(req, res) {
       if(err) { 
         console.log(err);
       } else {
-      res.send(doc);
+        res.redirect("/stories");
       }
+
   });
 });
 
